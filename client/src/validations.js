915476e69879f06e67d36acd13cbc14f1573a040
Swapnil Bandiwadekar <ssbandiwadekar@gmail.com>
2020-03-02 13:40:19 +0530

    Entire directory is made clean to make only one solution for desktop and cloud app

--- client/src/validations.js
+++
@@ -1,6 +0,0 @@
-const validations = {
-    onlyNumber: new RegExp(/((?![0-9]).)/g),
-    phoneNumber: new RegExp(/^[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]$/),
-}
-
-export default validations;
