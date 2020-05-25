5de23251cbce84350b16232193347a0df3064303
Swapnil <ssbandiwadekar@gmail.com>
2020-05-25 12:19:50 +0530

    committing to get changes from the remote.

--- PbdApp/app/src/main/java/com/example/pbdexecutives/MainActivity.kt
+++ PbdApp/app/src/main/java/com/example/pbdexecutives/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.pbdexecutives
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val handler = Handler();
+        handler.postDelayed({
+            var loggedIn: Boolean = false;
+            if(loggedIn) {
+                val intent = Intent(this, LoginActivity::class.java)
+                startActivity(intent);
+            }
+        }, 2000);
     }
 }
