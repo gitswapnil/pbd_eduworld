18a1bbd8c75edeccf205afa017efae9e7e6ecb74
Swapnil <ssbandiwadekar@gmail.com>
2020-05-28 06:46:55 +0530

    login.xml improved for scrollable view

--- PbdApp/app/src/main/java/com/example/pbdexecutives/LoginActivity.kt
+++ PbdApp/app/src/main/java/com/example/pbdexecutives/LoginActivity.kt
@@ -1,12 +1,25 @@
 package com.example.pbdexecutives
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.EditText
 
 class LoginActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+    }
+
+    fun login(view: View) {
+//        val phoneNo = findViewById<EditText>(R.id.editText2);
+//        val password = findViewById<EditText>(R.id.editText3);
+
+
+
+        val intent = Intent(this, HomeActivity::class.java)
+        startActivity(intent);
     }
 }
