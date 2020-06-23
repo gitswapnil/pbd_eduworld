6a5806a54695157a9b13ea27d22bd7343cc82f4d
swapnil <ssbandiwadekar@gmail.com>
2020-06-23 15:09:30 +0530

    Add new task activity only UI is in progress.

--- PbdApp/app/src/main/java/com/example/pbdexecutives/AddNewTaskActivity.kt
+++ PbdApp/app/src/main/java/com/example/pbdexecutives/AddNewTaskActivity.kt
@@ -2,10 +2,24 @@ package com.example.pbdexecutives
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 
 class AddNewTaskActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_new_task)
+
+        setSupportActionBar(findViewById(R.id.add_new_task_toolbar))
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
     }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == android.R.id.home) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
 }
